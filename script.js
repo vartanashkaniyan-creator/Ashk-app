2b651aa76a008fe1d599da6b778cbb0e9845548b
vartanashkaniyan-creator <vartanashkaniyan@gmail.com>
2025-12-28 15:18:24 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,18 @@
-body {
-  font-family: sans-serif;
-  background: #0f172a;
-  color: white;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-}
+function generateCode() {
+  const cmd = document.getElementById("command").value;
+  const out = document.getElementById("output");
 
-.app {
-  width: 90%;
-  max-width: 500px;
-  background: #020617;
-  padding: 20px;
-  border-radius: 12px;
-}
+  if (cmd.includes("note")) {
+    out.textContent =
+`// Android Java Code
+EditText note;
+Button save;
 
-h1 {
-  text-align: center;
-}
-
-textarea {
-  width: 100%;
-  height: 120px;
-  margin-top: 10px;
-  border-radius: 8px;
-  padding: 10px;
-  border: none;
-}
-
-button {
-  width: 100%;
-  margin-top: 10px;
-  padding: 12px;
-  border: none;
-  border-radius: 8px;
-  background: #22c55e;
-  font-size: 16px;
-}
-
-pre {
-  background: black;
-  margin-top: 10px;
-  padding: 10px;
-  border-radius: 8px;
-  overflow-x: auto;
+save.setOnClickListener(v -> {
+  // save note
+});`;
+  } 
+  else {
+    out.textContent = "// دستور شناخته نشد";
+  }
 }
